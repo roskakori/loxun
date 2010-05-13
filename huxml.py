867c2aa16143fed470575e54c22efe0dc509638f
Thomas Aglassinger <roskakori@users.sourceforge.net>
2010-05-13 09:45:15 +0000

    * Added version information. * Added license comment.

--- huxml.py
+++ huxml.py
@@ -91,9 +91,26 @@ Hello world!
 </xhtml:html>
 
 """
+# Copyright (C) 2010 Thomas Aglassinger
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import collections
 import os
 import xml.sax.saxutils
+
+VERSION = "0.1"
+REPOSITORY_ID, VERSION_DATE = "$Id$".split()[2:4]
 
 class XmlError(Exception):
     """
